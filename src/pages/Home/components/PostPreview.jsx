db4a540bf968550fa92b5b269d8d7cd03856e911
Choi Seung Ryeol <danielchoi1115@gmail.com>
2024-10-14 11:14:06 +0900

    PostPreview 추가

--- src/pages/Home/components/PostPreview.jsx
+++ src/pages/Home/components/PostPreview.jsx
@@ -1,13 +1,5 @@
 function PostPreview() {
-  return (
-    <div className="outline1" style={{ width: 300 }}>
-      <div className="outline3">PostPreview </div>
-      <div className="outline3" style={{ height: 300 }}>
-        사진
-      </div>
-      <div className="outline3"> 하단 </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default PostPreview;
